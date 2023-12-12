d4dd8845f986865deb36347ee9f640cc9f6b0c35
Ihor Poliakov <piv2003@gmail.com>
2023-12-12 22:02:31 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { GlobalStyle } from './GlobalStyle.jsx';
 import { ThemeProvider } from 'styled-components';
-import 'react-toastify/dist/ReactToastify.minimal.css';
+import { glodalColor } from './Styled/GlobalColor.jsx';
 import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import './index.css';
-import App from './App.jsx';
-import { glodalColor } from './Styled/GlobalColor.jsx';
-import { GlobalStyle } from './GlobalStyle.jsx';
-import { persistor, store } from './redux/store.js';
 import '../node_modules/swiper/modules';
 import '../node_modules/swiper/modules/grid.css';
 import '../node_modules/swiper/modules/pagination.css';
 import '../node_modules/swiper/swiper-bundle.css';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.minimal.css';
 import { StyledContainer } from './Styled/ToastStyles.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
