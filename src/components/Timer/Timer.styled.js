b75162323ad8fc0654fed4fd8d88cbefe161976b
Ihor Poliakov <piv2003@gmail.com>
2024-01-05 10:15:07 +0200

    Update Timer.styled.js

--- src/components/Timer/Timer.styled.js
+++ src/components/Timer/Timer.styled.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { glodalColor } from '../../Styled/GlobalColor';
 
-// ============TIMER=============
-
 export const BoxTimer = styled.div`
   display: flex;
   flex-direction: column;
